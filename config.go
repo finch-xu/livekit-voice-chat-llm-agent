cc648e70c93cded95e420b2fbb0ddf9f170130b3
虚拟世界的懒猫 <39703270+finch-xu@users.noreply.github.com>
2025-07-19 23:53:55 +0800

    init

---
+++ config.go
@@ -0,0 +1,19 @@
+package main
+
+const (
+	// tencent speech
+	appId     = ""
+	secretId  = ""
+	secretKey = ""
+
+	// 模型token
+	modelUrl   = "https://qianfan.gz.baidubce.com/v2/chat/completions"
+	modelToken = "Bearer 123"
+	// livekit
+	host                = "ws://192.168.124.98:7880"
+	apiKey              = ""
+	apiSecret           = ""
+	roomName            = "room01"
+	participantIdentity = "participant-server"
+)
+
